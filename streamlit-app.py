2d22dacddf628d79bf5b0f0002c6c7023ced9261
ivanvouffo123 <ivanvouffo123@gmail.com>
2025-01-24 15:19:12 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -40,8 +40,8 @@ with st.sidebar:
 
     if st.button("EDA", use_container_width=True, on_click=set_page_selection, args=('eda',)):
         st.session_state.page_selection = "eda"
-	    chart = alt.Chart(df).mark_point().encode(x='petal_length', y='petal_width', color="species")
-	    st.write(chart)
+	chart = alt.Chart(df).mark_point().encode(x='petal_length', y='petal_width', color="species")
+        st.write(chart)
 
     if st.button("Data Cleaning / Pre-processing", use_container_width=True, on_click=set_page_selection, args=('data_cleaning',)):
         st.session_state.page_selection = "data_cleaning"
